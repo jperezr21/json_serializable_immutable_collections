bdfde90118bda7c89f78c8fba22e206f4bcfcb78
Martin Kamleithner <martin+kamleithner@diagnosia.com>
2020-04-14 11:43:52 +0200

    add example

---
+++ example/main.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Model _$ModelFromJson(Map<String, dynamic> json) {
+  return Model(
+    myIntList: json['myIntList'] != null
+        ? KtList<int>.from((json['myIntList'] as List).map((e) => e as int))
+        : null,
+  );
+}
+
+Map<String, dynamic> _$ModelToJson(Model instance) => <String, dynamic>{
+      'myIntList': instance.myIntList?.asList(),
+    };
